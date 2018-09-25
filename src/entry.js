3e57e0712d85a24ce77d638179d5ffc29cb4080d
Aaron Cordova <theqodesmith@gmail.com>
2018-09-25 08:28:11 -0400

    Removing React Router stuff from entry file. Will live somewhere else later.

--- src/entry.js
+++ src/entry.js
@@ -14,28 +14,19 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 
 // Import our components.
-import Home from 'components/Home'
+import App from 'components/App'
 import NotFound from 'components/NotFound'
 
 // Import a store, created & ready to go.
 import store from './store'
 
-// Import React Router things.
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
 
 // Create a single element for our app to live.
 document.body.innerHTML = '<div id="app"></div>'
-document.body.className = 'bg-black-80 fw4 white-80'
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route component={NotFound} />
-      </Switch>
-    </Router>
+    <App />
   </Provider>,
   document.querySelector('#app')
 )
